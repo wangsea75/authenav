9d604063524eb9aba9d0fd1c8102dd724a420431
Haiyang Wang <1291466087@qq.com>
2016-12-14 17:04:12 +0800

    add h264 sign/verify sources

---
+++ sign_h264_file.c
@@ -0,0 +1,378 @@
+/*
+ * Copyright (c) 2013 Stefano Sabatini
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ * @file
+ * libavformat/libavcodec demuxing and muxing API example.
+ *
+ * Remux streams from one container format to another.
+ * @example remuxing.c
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <assert.h>
+
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/rsa.h>
+#include <openssl/sha.h>
+#include <openssl/rand.h>
+
+
+#define SIGNATURE_LEN 256
+
+const uint8_t uuid[16] = {0x13, 0xcf, 0xfd, 0x43, 0x20, 0x65, 0x40, 0xb6, 0xa4, 0x11, 0xd4, 0x75, 0x9b, 0x2c, 0xad, 0x68,};
+const char hn[] = "SHA256";
+
+typedef unsigned char byte;
+typedef struct{
+	uint8_t  len[4];
+	uint8_t  header;
+	uint8_t  sei_type;
+	uint8_t  uuid[16];
+	uint8_t  sig[SIGNATURE_LEN];
+}SIG_SEI_NAL;
+
+void init_sig_sei_nal(SIG_SEI_NAL *nal)
+{
+	//nal->len = 2 + sizeof(nal->uuid) + sizeof(nal->sig);
+	nal->len[0] = 0x00; //big endian 274
+	nal->len[1] = 0x00;
+	nal->len[2] = 0x01;
+	nal->len[3] = 0x12;
+	nal->header = 0x06;
+	nal->sei_type = 0x05;
+	memcpy(nal->uuid, uuid, 16);
+	memset(nal->sig, 0x00, sizeof(nal->sig));
+}
+
+int sign_it(const byte* msg, size_t mlen, byte** sig, size_t* slen, EVP_PKEY* pkey)
+{
+    /* Returned to caller */
+    int result = -1;
+
+    if(!msg || !mlen || !sig || !pkey) {
+        assert(0);
+        return -1;
+    }
+
+    if(*sig)
+        OPENSSL_free(*sig);
+
+    *sig = NULL;
+    *slen = 0;
+
+    EVP_MD_CTX* ctx = NULL;
+
+    do
+    {
+        ctx = EVP_MD_CTX_create();
+        assert(ctx != NULL);
+        if(ctx == NULL) {
+            printf("EVP_MD_CTX_create failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        const EVP_MD* md = EVP_get_digestbyname(hn);
+        assert(md != NULL);
+        if(md == NULL) {
+            printf("EVP_get_digestbyname failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        int rc = EVP_DigestInit_ex(ctx, md, NULL);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestInit_ex failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        rc = EVP_DigestSignInit(ctx, NULL, md, NULL, pkey);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignInit failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        rc = EVP_DigestSignUpdate(ctx, msg, mlen);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignUpdate failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        size_t req = 0;
+        rc = EVP_DigestSignFinal(ctx, NULL, &req);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed (1), error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        assert(req > 0);
+        if(!(req > 0)) {
+            printf("EVP_DigestSignFinal failed (2), error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        *sig = OPENSSL_malloc(req);
+        assert(*sig != NULL);
+        if(*sig == NULL) {
+            printf("OPENSSL_malloc failed, error 0x%lx\n", ERR_get_error());
+            break; /* failed */
+        }
+
+        *slen = req;
+        rc = EVP_DigestSignFinal(ctx, *sig, slen);
+        assert(rc == 1);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed (3), return code %d, error 0x%lx\n", rc, ERR_get_error());
+            break; /* failed */
+        }
+
+        assert(req == *slen);
+        if(rc != 1) {
+            printf("EVP_DigestSignFinal failed, mismatched signature sizes %ld, %ld", req, *slen);
+            break; /* failed */
+        }
+
+        result = 0;
+
+    } while(0);
+
+    if(ctx) {
+        EVP_MD_CTX_destroy(ctx);
+        ctx = NULL;
+    }
+
+    return !!result;
+}
+
+void print_it(const char* label, const byte* buff, size_t len)
+{
+    size_t i;
+	if(!buff || !len)
+        return;
+
+    if(label)
+        printf("%s: ", label);
+
+    for(i=0; i < len; ++i)
+        printf("%02X", buff[i]);
+
+    printf("\n");
+}
+
+static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt, const char *tag)
+{
+    AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
+
+    printf("%s: pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
+           tag,
+           av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
+           av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
+           av_ts2str(pkt->duration), av_ts2timestr(pkt->duration, time_base),
+           pkt->stream_index);
+}
+
+static uint8_t *append(uint8_t *buf, const uint8_t *src, int size)
+{
+   memcpy(buf, src, size);
+   return buf + size;
+}
+
+int main(int argc, char **argv)
+{
+    AVOutputFormat *ofmt = NULL;
+    AVFormatContext *ifmt_ctx = NULL, *ofmt_ctx = NULL;
+    AVPacket pkt;
+    const char *in_filename, *out_filename;
+    int ret, i;
+
+    EVP_PKEY *skey = NULL, *vkey = NULL;
+	uint8_t* sig = NULL;
+    size_t slen = 0;
+	FILE *kfile = NULL;
+	int rc;
+
+	SIG_SEI_NAL nal;
+	init_sig_sei_nal(&nal);
+
+    if (argc != 4) {
+        printf("usage: %s <input file> <output file> <sign key file>\n", argv[0]);
+        return 1;
+    }
+
+    in_filename  = argv[1];
+    out_filename = argv[2];
+
+    av_register_all();
+
+    if ((ret = avformat_open_input(&ifmt_ctx, in_filename, 0, 0)) < 0) {
+        fprintf(stderr, "Could not open input file '%s'", in_filename);
+        goto end;
+    }
+
+    if ((ret = avformat_find_stream_info(ifmt_ctx, 0)) < 0) {
+        fprintf(stderr, "Failed to retrieve input stream information");
+        goto end;
+    }
+
+    av_dump_format(ifmt_ctx, 0, in_filename, 0);
+
+    avformat_alloc_output_context2(&ofmt_ctx, NULL, NULL, out_filename);
+    if (!ofmt_ctx) {
+        fprintf(stderr, "Could not create output context\n");
+        ret = AVERROR_UNKNOWN;
+        goto end;
+    }
+
+    ofmt = ofmt_ctx->oformat;
+
+    for (i = 0; i < ifmt_ctx->nb_streams; i++) {
+        AVStream *in_stream = ifmt_ctx->streams[i];
+        AVStream *out_stream = avformat_new_stream(ofmt_ctx, in_stream->codec->codec);
+        if (!out_stream) {
+            fprintf(stderr, "Failed allocating output stream\n");
+            ret = AVERROR_UNKNOWN;
+            goto end;
+        }
+
+        ret = avcodec_copy_context(out_stream->codec, in_stream->codec);
+        if (ret < 0) {
+            fprintf(stderr, "Failed to copy context from input to output stream codec context\n");
+            goto end;
+        }
+        out_stream->codec->codec_tag = 0;
+        if (ofmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+            out_stream->codec->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+    }
+    av_dump_format(ofmt_ctx, 0, out_filename, 1);
+
+    if (!(ofmt->flags & AVFMT_NOFILE)) {
+        ret = avio_open(&ofmt_ctx->pb, out_filename, AVIO_FLAG_WRITE);
+        if (ret < 0) {
+            fprintf(stderr, "Could not open output file '%s'", out_filename);
+            goto end;
+        }
+    }
+
+    ret = avformat_write_header(ofmt_ctx, NULL);
+    if (ret < 0) {
+        fprintf(stderr, "Error occurred when opening output file\n");
+        goto end;
+    }
+
+    //
+
+/*    AVPacket tmp_pkt;
+    uint8_t sei[] = {0x00, 0x00, 0x00, 0x16, 0x06, 0x05,
+    				 0x13, 0xcf, 0xfd, 0x43, 0x20, 0x65, 0x40, 0xb6, 0xa4, 0x11, 0xd4, 0x75, 0x9b, 0x2c, 0xad, 0x68,
+    				 'a', 'b', 'c', 'd'};*/
+
+    /*read sign key from a PEM file*/
+	if( (kfile = fopen(argv[3], "rt")) == NULL)
+		exit(1);
+	skey = PEM_read_PrivateKey(kfile, NULL, NULL, NULL);
+
+	/*initialize sign and verify keys*/
+	OpenSSL_add_all_algorithms();
+
+	assert(skey != NULL);
+	if(skey == NULL)
+		exit(1);
+
+	size_t org_pkt_size; //save original packet size
+
+    while (1) {
+        AVStream *in_stream, *out_stream;
+
+        ret = av_read_frame(ifmt_ctx, &pkt);
+        if (ret < 0)
+            break;
+
+        in_stream  = ifmt_ctx->streams[pkt.stream_index];
+        out_stream = ofmt_ctx->streams[pkt.stream_index];
+        log_packet(ifmt_ctx, &pkt, "in");
+
+        org_pkt_size = pkt.size;
+
+        /*grow packet to save signature*/
+        av_grow_packet(&pkt, sizeof(SIG_SEI_NAL));
+
+        /* sign the data in the packet read */
+        rc = sign_it(pkt.data, org_pkt_size, &sig, &slen, skey);
+		assert(rc == 0);
+		if(rc == 0) {
+			printf("Created signature\n");
+		} else {
+			printf("Failed to create signature, return code %d\n", rc);
+			exit(1);  //Should cleanup here
+		}
+
+		/*append signature sei nal*/
+		memcpy(nal.sig, sig, slen);
+		memcpy(pkt.data + org_pkt_size, &nal, sizeof(SIG_SEI_NAL));
+
+		print_it("Signature", sig, slen);
+
+        //memcpy(pkt.data + pkt.size - sizeof(sei), sei, sizeof(sei));
+        //memcpy(pkt.data + pkt.size - sizeof(SIG_SEI_NAL), &nal, 278);//sizeof(SIG_SEI_NAL));
+
+        /*copy packet*/
+        pkt.pts = av_rescale_q_rnd(pkt.pts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+        pkt.dts = av_rescale_q_rnd(pkt.dts, in_stream->time_base, out_stream->time_base, AV_ROUND_NEAR_INF|AV_ROUND_PASS_MINMAX);
+        pkt.duration = av_rescale_q(pkt.duration, in_stream->time_base, out_stream->time_base);
+        pkt.pos = -1;
+        log_packet(ofmt_ctx, &pkt, "out");
+
+        ret = av_interleaved_write_frame(ofmt_ctx, &pkt);
+        if (ret < 0) {
+            fprintf(stderr, "Error muxing packet\n");
+            break;
+        }
+        av_packet_unref(&pkt);
+    }
+
+    av_write_trailer(ofmt_ctx);
+end:
+
+    avformat_close_input(&ifmt_ctx);
+
+    /* close output */
+    if (ofmt_ctx && !(ofmt->flags & AVFMT_NOFILE))
+        avio_closep(&ofmt_ctx->pb);
+    avformat_free_context(ofmt_ctx);
+
+    if (ret < 0 && ret != AVERROR_EOF) {
+        fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+        return 1;
+    }
+
+    return 0;
+}
